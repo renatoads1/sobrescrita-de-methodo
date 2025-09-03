890a324f040f09a36edad640fd20185a2c9341ff
Renato de Azevedo Lacerda <renatoads1@gmail.com>
2025-09-03 07:51:01 -0300

    Adicionar arquivos de projeto.

---
+++ sobrescrita/Program.cs
@@ -0,0 +1,41 @@
+﻿
+
+var animais = new List<Animal>
+{
+    new Animal(),
+    new Cachorro(),
+    new Gato()
+};
+
+foreach (var animal in animais)
+{
+    animal.EmitirSom();
+}
+
+//O animal faz um som.
+//cachorro late
+//gato mia
+
+public class Animal
+{
+    public virtual void EmitirSom()
+    {
+        Console.WriteLine("O animal faz um som.");
+    }
+}
+
+public class Cachorro : Animal
+{
+    public override void EmitirSom()
+    {
+        Console.WriteLine("O cachorro late.");
+    }
+}
+
+public class Gato : Animal
+{
+    public override void EmitirSom()
+    {
+        Console.WriteLine("O gato mia.");
+    }
+}
